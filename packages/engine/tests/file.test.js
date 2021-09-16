d8330c4bfcf0fdef3ac39f53c5c8b17345304430
chenyuan <cy476571@gmail.com>
2021-09-16 17:03:49 +0800

    add testcase for readdirs func

--- packages/engine/tests/file.test.js
+++ packages/engine/tests/file.test.js
@@ -12,4 +12,11 @@ test('Get excluded dirs from ignoreDirs', () => {
   const excludeDirs = fileHandlers.getExcludedDirs(config)
   expect(excludeDirs.toString()).toBe('[object Set]')
   expect(excludeDirs.has('.git')).toBe(true)
-});
+})
+
+test('Make sure readDirs function always return array []', async () => {
+  const excludeDirs = new Set()
+  const allFiles = await fileHandlers.readDirs('./xxx', excludeDirs)
+  expect(Array.isArray(allFiles)).toBe(true)
+  expect(allFiles.length).toBe(0)
+})
